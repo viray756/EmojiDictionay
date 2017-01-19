52af6818a3c58aa5c4cab890424d661f56d2fedd
Jose Viray <viray756@gmail.com>
2017-01-18 22:15:47 -0500

    Tableviews and Arrays

--- EmojiDictionay/ViewController.swift
+++ EmojiDictionay/ViewController.swift
@@ -12,24 +12,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var emojiTable: UITableView!
     
+    var emojis = ["😀","😍","🙏","👿","👻","👀","🐼","🙊"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Ask screen in the beginning what to look for
         emojiTable.dataSource = self
         emojiTable.delegate = self
         
     }
 
-    
+    //Set how many rows need to be in TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
+    
+    //What to populate in each row(cell)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
         let cell = UITableViewCell()
-        cell.textLabel?.text = "😀"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     
