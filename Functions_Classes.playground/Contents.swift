e3e0967533fd5490e019d8e7c3148221f2962d87
Jose Viray <viray756@gmail.com>
2017-01-19 20:15:26 -0500

    no message

--- Functions_Classes.playground/Contents.swift
+++ Functions_Classes.playground/Contents.swift
@@ -1,5 +1,20 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
 
-var str = "Hello, playground"
+func hello() {
+    print("Hello World!")
+}
+
+hello()
+
+func printItOut(word :String) {
+    print(word)
+}
+
+printItOut(word: "WHats Up")
+
+func addTwo(num1 :Int, num2 :Int){
+    print(num1 + num2)
+    
+}
+
+addTwo(num1: 2, num2: 5)
